7748e6b79c186dbdd16ef458a0533e6a1631b712
yashdarji30 <123467783+yashdarji30@users.noreply.github.com>
2024-08-15 21:48:58 +0530

    login,signup backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      staleTimes: {
+        dynamic: 30,
+      },
+    },
+    serverExternalPackages: ["@node-rs/argon2"],
+  };
+  
+  export default nextConfig;
---
+++ src/lib/validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const requiredString = z.string().trim().min(1, "Required");
+export const signUpSchema = z.object({
+  email: requiredString.email("Invalid email"),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]+$/,
+    "Only letters, numbers, underscores, and dashes allowed",
+  ),
+  password: requiredString.min(8, "Password must be at least 8 characters"),
+});
+
+export type SignUpValues = z.infer<typeof signUpSchema>;
+
+export const loginSchema = z.object({
+  username: requiredString,
+  password: requiredString,
+});
+
+export type LoginValues = z.infer<typeof loginSchema>;
