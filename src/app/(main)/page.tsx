3b654d49954dbbfd6324794c6f7c554be67e1a57
yashdarji30 <123467783+yashdarji30@users.noreply.github.com>
2024-08-16 18:28:46 +0530

    DarkMode and Responsive Sticky sidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>ForntPage</main>
+  return <main 
+  className="h-[200vh] w-full bg-red-50"
+  >
+    <div className="w-full">ForntPage</div>
+    </main>
 }
